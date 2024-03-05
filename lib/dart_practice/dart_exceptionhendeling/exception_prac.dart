0dd0179012594f9e40617b82b800e2676606a664
kamru <kamrulhasan4947@gmail.com>
2024-03-06 00:11:34 +0600

    exception

---
+++ lib/dart_practice/dart_exceptionhendeling/exception_prac.dart
@@ -0,0 +1,7 @@
+/*
+*  Exceptions:
+*  Exceptions are errors indicating that some things unexpected ,
+*  the exception isn't caught, the isolate that raised the exception is suspended,
+*  and typically the isolate and its program are terminated.
+*/
+
