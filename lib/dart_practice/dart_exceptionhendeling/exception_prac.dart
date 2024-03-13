c2b8e4a0c0fc95e7efe2fbfce7a4f78aaa3e5ccb
kamru <kamrulhasan4947@gmail.com>
2024-03-13 23:33:32 +0600

    minimal2

--- lib/dart_practice/dart_exceptionhendeling/exception_prac.dart
+++ lib/dart_practice/dart_exceptionhendeling/exception_prac.dart
@@ -5,44 +5,6 @@
 *  and typically the isolate and its program are terminated.
 */
 
-
-import 'dart:io';
-
-import 'package:dart_project/dart_practice/ascync_prog/asyncprog.dart';
-
-import 'dart_practice/dart_exceptionhendeling/exception_prac.dart';
-
-
-
-void main(){
-  print('''
-        ---------------------------------------------------------------------------------                                             
-       |                                                                                 |
-       |                     Welcome To Dart Practice project.                           |
-       |                                                                                 |
-        ---------------------------------------------------------------------------------
-       |  *Enter any of them topics: (ex:  1  - means 'variable')                                      |
-       |                                                                                 |
-       |  1. Data Types and Variables                7. Inheritance                      |       
-       |  2. Control statements- conditions          8. Encapsulation                    |
-       |  3. Control statements- loops               9. abstraction & abstract           |
-       |  4. Functions                               10. Interface & implementations     |
-       |  5. OOP ( Class & object)                   11. Exception Handling              |             
-       |  6.Methode & properties                     12. Ascync Programing               |                   
-       |                                                                                 |   
-        ---------------------------------------------------------------------------------                                                     
-  ''');
-
-  print('Enter start program number from above:( 1  means variable)\n');
-  int? startprogram = int.tryParse(stdin.readLineSync()!);
-  switch(startprogram){
-    case 11:
-      exStarting();
-    case 12:
-      ascyncStarting();
-    default:
-      print('This Topic is not added yet!');
-  }
 void exStarting(){
   print('''
   
